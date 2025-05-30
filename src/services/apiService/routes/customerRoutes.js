9e8ea1b55cf7d6c43593842f7ca9490670623732
Olivia Chapman <olivia.chapman@capycino-studios.com>
2025-05-30 11:34:14 +0100

    Added CRUD page for customers

--- src/services/apiService/routes/customerRoutes.js
+++ src/services/apiService/routes/customerRoutes.js
@@ -8,4 +8,28 @@ export default class {
             return res.data;
         })
     }
+
+    async fetchStats() {
+        return await this.axiosInstance.get('/customers/fetch/stats').then((res) => {
+            return res.data;
+        })
+    }
+
+    async createCustomer(form) {
+        return await this.axiosInstance.post('/customers/create', form).then((res) => {
+            return res.data;
+        })
+    }
+
+    async updateCustomer(id, form) {
+        return await this.axiosInstance.put(`/customers/update/${id}`, form).then((res) => {
+            return res.data;
+        })
+    }
+
+    async deleteCustomer(id) {
+        return await this.axiosInstance.delete(`/customers/delete/${id}`).then((res) => {
+            return res.data;
+        })
+    }
 }
